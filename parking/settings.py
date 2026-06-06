72dd9a895a7323fe2368e74314bffa48ef39bc7e
ariel363454 <kirito1070100@gmail.com>
2026-06-06 20:15:10 +0800

    Update settings.py

--- parking/settings.py
+++ parking/settings.py
@@ -62,6 +62,9 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'ssl': {
+                'ssl_mode': 'REQUIRED'
+            }
         }
     }
 }
